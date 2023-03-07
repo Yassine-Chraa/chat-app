3dcf5f7c2d3a32e8db5cce28f687f2d3aca46fde
Yassine-Chraa <yassinechraa1@gmail.com>
2023-03-07 23:39:09 +0100

    update style

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -5,7 +5,8 @@ import { useLocation } from "react-router-dom";
 import ScrollToBottom from "react-scroll-to-bottom";
 import ReactEmoji from "react-emoji";
 import onlineIcon from "../../icons/onlineIcon.png";
-import closeIcon from "../../icons/closeIcon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faSignOut } from "@fortawesome/free-solid-svg-icons";
 import "./chat.css";
 const ENDPOINT = "http://localhost:5000/";
 const socket = io(ENDPOINT);
@@ -59,76 +60,93 @@ const Chat = () => {
     }
   };
   return (
-    <div className="outerContainer">
+    <div className="container">
       <div className="container">
         <div className="infoBar">
           <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+            <FontAwesomeIcon
+              icon={faCircle}
+              color="blue"
+              style={{ fontSize: 10, marginRight: 12 }}
+            />
             <h3>{room}</h3>
           </div>
           <div className="rightInnerContainer">
             <a href="/">
-              <img src={closeIcon} alt="close icon" />
+              <FontAwesomeIcon icon={faSignOut} color="#fff" />
             </a>
           </div>
         </div>
-        <ScrollToBottom className="messages">
-          {messages.map((message, i) => (
-            <div key={i}>
-              {isSentByCurrentUser ? (
-                <div className="messageContainer justifyEnd">
-                  <p className="sentText pr-10">{trimmedName}</p>
-                  <div className="messageBox backgroundBlue">
-                    <p className="messageText colorWhite">
-                      {ReactEmoji.emojify(message.text)}
-                    </p>
+        <div className="row">
+          <ScrollToBottom className="messages">
+            {messages.map((message, i) => (
+              <div key={i}>
+                {isSentByCurrentUser ? (
+                  <div className="messageContainer justifyEnd">
+                    <p className="sentText pr-10">{trimmedName}</p>
+                    <div className="messageBox backgroundBlue">
+                      <p className="messageText colorWhite">
+                        {ReactEmoji.emojify(message.text)}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ) : (
-                <div className="messageContainer justifyStart">
-                  <div className="messageBox backgroundLight">
-                    <p className="messageText colorDark">
-                      {ReactEmoji.emojify(message.text)}
-                    </p>
+                ) : (
+                  <div className="messageContainer justifyStart">
+                    <div className="messageBox backgroundLight">
+                      <p className="messageText colorDark">
+                        {ReactEmoji.emojify(message.text)}
+                      </p>
+                    </div>
+                    <p className="sentText pl-10 ">{message.user}</p>
                   </div>
-                  <p className="sentText pl-10 ">{message.user}</p>
-                </div>
-              )}
-            </div>
-          ))}
-        </ScrollToBottom>
+                )}
+              </div>
+            ))}
+          </ScrollToBottom>
+        </div>
         <form className="form">
-          <input
-            className="input"
-            type="text"
-            placeholder="Type a message..."
-            value={message}
-            onChange={({ target: { value } }) => setMessage(value)}
-            onKeyPress={(event) =>
-              event.key === "Enter" ? sendMessage(event) : null
-            }
-          />
-          <button className="sendButton" onClick={(e) => sendMessage(e)}>
-            Send
-          </button>
-        </form>
-      </div>
-      <div className="textContainer">
-        {users ? (
-          <div>
-            <h1>People currently chatting:</h1>
-            <div className="activeContainer">
-              <h2>
-                {users.map(({ name }) => (
-                  <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={onlineIcon} />
-                  </div>
-                ))}
-              </h2>
+          <div className="row">
+            <div className="col-md-8 mx-0">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Type a message..."
+                value={message}
+                onChange={({ target: { value } }) => setMessage(value)}
+                onKeyPress={(event) =>
+                  event.key === "Enter" ? sendMessage(event) : null
+                }
+              />
+            </div>
+            <div className="col-md-4 mt-2">
+              <button
+                className="btn btn-primary"
+                style={{ width: "100%" }}
+                onClick={(e) => sendMessage(e)}
+              >
+                Send
+              </button>
             </div>
           </div>
-        ) : null}
+        </form>
+      </div>
+      <div className="row" style={{marginTop: 64}}>
+        <div className="col-12 mx-2">
+          {users ? (
+            <div>
+              <h1>People currently chatting:</h1>
+              <div className="activeContainer">
+                <ul class="list-group">
+                  {users.map(({ name }) => (
+                    <li class="list-group-item" key={name}>
+                      {name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
--- client/src/components/Start/index.js
+++ client/src/components/Start/index.js
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import './join.css';
+import "./join.css";
 
 export default function SignIn() {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
   return (
-    <div className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
-        <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+    <div className="joinOuterContainer row">
+      <div className="col-md-4 col-sm-6 p-0">
+        <div className="card p-4" style={{boxShadow: '1px 3px 9px #00000047'}}>
+          <h1 className="heading">Join</h1>
+          <form>
+            <div class="form-group mb-4">
+              <input
+                placeholder="Name"
+                className="form-control"
+                type="text"
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <div class="form-group mb-2">
+              <input
+                placeholder="Room"
+                className="form-control"
+                type="text"
+                onChange={(event) => setRoom(event.target.value)}
+              />
+            </div>
+            <Link
+              onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+              to={`/chat?name=${name}&room=${room}`}
+            >
+              <button className="btn btn-primary" type="submit">
+                Sign In
+              </button>
+            </Link>
+          </form>
         </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
-        </div>
-        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
-        </Link>
       </div>
     </div>
   );
